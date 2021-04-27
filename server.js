dd80a27368ad292897e588926ac20c5358ebe25f
Karan Mishra <karan.m2704@gmail.com>
2021-04-27 23:20:13 +0530

    Add Server Code

---
+++ server.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+const express = require('express')
+
+
+const app = express()
+
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+//headers: { 
+//   'Cookie': '__cfduid=dfa28b859f8529de5cba75dfbce40e4e11619455947'
+// }
+
+app.get('/', function(req, res){
+
+    var config = {
+        method: 'get',
+        url: 'https://api.wazirx.com/api/v2/tickers',
+        
+      };
+    axios(config)
+    .then(function (response) {
+    
+        res.send(JSON.stringify(response.data))
+        
+})
+.catch(function (error) {
+  console.log(error);
+});
+})
+
+
+
+
+
+
+app.listen(8080, function() {
+    console.log('Running on port 8080');
+})
